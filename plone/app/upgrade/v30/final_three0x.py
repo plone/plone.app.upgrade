28cacd510f32c6a548e23b57ff526528c4f21541
Hanno Schlichting <hanno@hannosch.eu>
2008-11-09 15:43:41 +0000

    Snapshot after moving code and profiles
    svn path=/plone.app.upgrade/trunk/; revision=23221

---
+++ plone/app/upgrade/v30/final_three0x.py
@@ -0,0 +1,25 @@
+from Products.CMFCore.utils import getToolByName
+from Products.CMFEditions.StandardModifiers import install
+
+from Products.CMFPlone.migrations import logger
+from Products.CMFPlone.migrations.migration_util import loadMigrationProfile
+from Products.CMFPlone.migrations.v3_0.alphas import registerToolsAsUtilities
+
+
+def final_three01(context):
+    loadMigrationProfile(context, 'profile-Products.CMFPlone.migrations:3.0final-3.0.1')
+
+
+def three01_three02(context):
+    loadMigrationProfile(context, 'profile-Products.CMFPlone.migrations:3.0.1-3.0.2')
+
+
+def three03_three04(context):
+    loadMigrationProfile(context, 'profile-Products.CMFPlone.migrations:3.0.3-3.0.4')
+
+
+def installNewModifiers(context):
+    modifiers = getToolByName(context, 'portal_modifier', None)
+    if modifiers is not None:
+        install(modifiers)
+        logger.info('Added new CMFEditions modifiers.')
