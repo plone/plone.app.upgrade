06969b50f51f09d9a283acb0689b61775cfcf483
David Glick <davidglick@groundwire.org>
2009-09-09 03:15:46 +0000

    give a more helpful error for sites upgraded from Plone 2.1, refs #8802
    svn path=/plone.app.upgrade/branches/1.0/; revision=29610

--- plone/app/upgrade/__init__.py
+++ plone/app/upgrade/__init__.py
@@ -1,7 +1,9 @@
+import sys
 from zope.interface import implements
 from Products.CMFQuickInstallerTool.interfaces import INonInstallable
 
 class HiddenProducts(object):
+    """This hides the upgrade profiles from the quick installer tool."""
     implements(INonInstallable)
 
     def getNonInstallableProducts(self):
@@ -13,3 +15,12 @@ class HiddenProducts(object):
             'plone.app.upgrade.v33',
             'plone.app.upgrade.v40',
             ]
+
+# Make sure folks upgrading from Plone 2.1 see a helpful message telling them
+# how to do a two-stage upgrade, instead of a GroupUserFolder error.
+try:
+    from Products.GroupUserFolder.GroupUserFolder import GroupUserFolder
+except ImportError:
+    from plone.app.upgrade import gruf_bbb
+    sys.modules['Products.GroupUserFolder'] = gruf_bbb
+    sys.modules['Products.GroupUserFolder.GroupUserFolder'] = gruf_bbb
