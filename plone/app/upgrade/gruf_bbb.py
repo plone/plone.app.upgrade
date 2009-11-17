2883bb222e43cc87becf5f8d496e9cb035e8688a
Hanno Schlichting <hanno@hannosch.eu>
2009-11-17 15:50:41 +0000

    Prepare release
    svn path=/plone.app.upgrade/trunk/; revision=31760

--- plone/app/upgrade/gruf_bbb.py
+++ plone/app/upgrade/gruf_bbb.py
@@ -14,7 +14,7 @@ class GroupUserFolder(Persistent):
 class UpgradeProcessErrorView(BrowserView):
     
     def __call__(self):
-        return ('Upgrades from Plone < 2.5 are not supported.  Please do a '
-                'two-stage upgrade by upgrading to Plone 3 first.  See '
+        return ('Upgrades from Plone < 2.5 are not supported. Please do a '
+                'two-stage upgrade by upgrading to Plone 3 first. See '
                 '<a href="http://plone.org/upgrade">the upgrade '
                 'manual</a> for details.')
