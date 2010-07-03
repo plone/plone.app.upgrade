037dfe13f24d673392da2b5d734970ae78ba2421
Hanno Schlichting <hanno@hannosch.eu>
2010-07-03 23:27:26 +0000

    Import ``findObjects`` function from plone.app.folder.
    svn path=/plone.app.upgrade/trunk/; revision=37623

--- plone/app/upgrade/v40/alphas.py
+++ plone/app/upgrade/v40/alphas.py
@@ -431,19 +431,7 @@ def migrateFolders(context):
     MigrationView(portal, None)()
 
 
-def findObjects(origin):
-    """ generator to recursively find and yield all zope objects below
-        the given start point """
-    traverse = origin.unrestrictedTraverse
-    base = '/'.join(origin.getPhysicalPath())
-    cut = len(base) + 1
-    paths = [base]
-    for idx, path in enumerate(paths):
-        obj = traverse(path)
-        yield path[cut:], obj
-        if hasattr(aq_base(obj), 'objectIds'):
-            for id in obj.objectIds():
-                paths.insert(idx + 1, path + '/' + id)
+from plone.app.folder.utils import findObjects
 
 
 def recompilePythonScripts(context):
