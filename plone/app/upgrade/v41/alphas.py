3b526df9b96ad6be07289c42083ade617803ef67
Eric Steele <esteele@plone.org>
2011-02-14 14:32:45 +0000

    Now alpha 3.
    svn path=/plone.app.upgrade/trunk/; revision=47562

--- plone/app/upgrade/v41/alphas.py
+++ plone/app/upgrade/v41/alphas.py
@@ -1,5 +1,14 @@
+import transaction
+from BTrees.IIBTree import IIBTree, IISet, IITreeSet
+from BTrees.OIBTree import OIBTree
 from Products.CMFCore.utils import getToolByName
 from Products.GenericSetup.rolemap import RolemapExportConfigurator
+from Products.PluginIndexes.BooleanIndex.BooleanIndex import BooleanIndex
+from Products.PluginIndexes.DateRangeIndex.DateRangeIndex import DateRangeIndex
+from Products.PluginIndexes.FieldIndex.FieldIndex import FieldIndex
+from Products.PluginIndexes.KeywordIndex.KeywordIndex import KeywordIndex
+from Products.PluginIndexes.UUIDIndex.UUIDIndex import UUIDIndex
+
 from plone.app.upgrade.utils import loadMigrationProfile
 
 
@@ -131,3 +140,98 @@ def install_outputfilters(context):
 
 def to41alpha2(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:to41alpha2')
+
+
+def convert_to_booleanindex(catalog, index):
+    if isinstance(index, BooleanIndex):
+        return
+    index.__class__ = BooleanIndex
+    index._p_changed = True
+    catalog._catalog._p_changed = True
+    # convert _unindex from IOBTree to IIBTree
+    old_unindex = index._unindex
+    if not isinstance(old_unindex, IIBTree):
+        index._unindex = _unindex = IIBTree()
+        for k, v in old_unindex.items():
+            # docid to value (True, False)
+            _unindex[k] = int(bool(v))
+        del old_unindex
+        transaction.savepoint(optimistic=True)
+    # convert _index from OOBTree to IITreeSet
+    old_index = index._index
+    if not isinstance(old_index, IITreeSet):
+        index._index = _index = IITreeSet()
+        for k, v in old_index.items():
+            # value to docid (int or treeset)
+            if bool(k):
+                # only true values get into the new _index
+                if isinstance(v, int):
+                    _index.add(v)
+                else:
+                    _index.update(v)
+        del old_index
+        transaction.savepoint(optimistic=True)
+
+
+def convert_to_uuidindex(catalog, index):
+    if isinstance(index, UUIDIndex):
+        return
+    index.__class__ = UUIDIndex
+    index._p_changed = True
+    catalog._catalog._p_changed = True
+    # convert from OOBTree to OIBTree
+    old_index = index._index
+    if not isinstance(old_index, OIBTree):
+        index._index = _index = OIBTree()
+        for k, v in old_index.items():
+            if isinstance(v, int):
+                _index[k] = v
+            else:
+                if isinstance(v, (IISet, IITreeSet)):
+                    # inconsistent data, one uid with multiple docids
+                    _index[k] = v[0]
+                logger.error('Inconsistent UID index, UID %s is associated '
+                    'with multiple docids.' % k)
+        del old_index
+        transaction.savepoint(optimistic=True)
+
+
+def optimize_rangeindex(index):
+    # migrate internal IISet to IITreeSet
+    for name in ('_since', '_since_only', '_until', '_until_only'):
+        tree = getattr(index, name, None)
+        if tree is not None:
+            for k, v in tree.items():
+                if isinstance(v, IISet):
+                    tree[k] = IITreeSet(v)
+            transaction.savepoint(optimistic=True)
+
+
+def optimize_unindex(index):
+    # avoid using a simple int and always use a treeset instead to
+    # allow conflict resolution inside the treeset to happen
+    _index = getattr(index, '_index', None)
+    if _index is not None:
+        for k, v in _index.items():
+            if isinstance(v, int):
+                _index[k] = IITreeSet((v, ))
+        transaction.savepoint(optimistic=True)
+
+
+def optimize_indexes(context):
+    catalog = getToolByName(context, 'portal_catalog')
+    for index in catalog.getIndexObjects():
+        index_id = index.getId()
+        if isinstance(index, DateRangeIndex):
+            optimize_rangeindex(index)
+        elif index_id in ('is_default_page', 'is_folderish'):
+            convert_to_booleanindex(catalog, index)
+        elif index_id == 'UID':
+            convert_to_uuidindex(catalog, index)
+        elif isinstance(index, (FieldIndex, KeywordIndex)):
+            optimize_unindex(index)
+
+
+def to41alpha3(context):
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:to41alpha3')
+    optimize_indexes(context)
