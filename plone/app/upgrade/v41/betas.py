a1425b0a1878c9f5063963e3e51b98d75844afbf
Eric Steele <esteele@plone.org>
2011-03-06 21:43:55 +0000

    Bump version.
    svn path=/plone.app.upgrade/trunk/; revision=47941

--- plone/app/upgrade/v41/betas.py
+++ plone/app/upgrade/v41/betas.py
@@ -3,3 +3,6 @@ from plone.app.upgrade.utils import loadMigrationProfile
 
 def to41beta1(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:to41beta1')
+
+def to41beta2(context):
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:to41beta2')
