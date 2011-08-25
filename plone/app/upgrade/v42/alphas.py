9f7fad942b3f50c79354a9f963af1b1692cd1ce4
Eric Steele <esteele@plone.org>
2011-08-25 15:21:13 +0000

    Add profile for 4.2a2 upgrade.
    svn path=/plone.app.upgrade/trunk/; revision=51777

--- plone/app/upgrade/v42/alphas.py
+++ plone/app/upgrade/v42/alphas.py
@@ -8,3 +8,9 @@ def to42alpha1(context):
     """4.1 -> 4.2alpha1
     """
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v42:to42alpha1')
+
+
+def to42alpha2(context):
+    """4.2a1 -> 4.2a2
+    """
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v42:to42alpha2')
