1bdbc827c03519cae728fa41575981a4d926bec6
Stefan H. Holek <stefan@epy.co.at>
2011-10-05 11:30:03 +0200

    Add an upgrade step to go with the changeOwnershipOf fix.

--- plone/app/upgrade/v42/betas.py
+++ plone/app/upgrade/v42/betas.py
@@ -1,7 +1,21 @@
 import logging
+
 from plone.app.upgrade.utils import loadMigrationProfile
+from Products.CMFCore.utils import getToolByName
 
 logger = logging.getLogger('plone.app.upgrade')
+
+
+def fixOwnerTuples(portal):
+    # Repair owner tuples that contain the memberdata tool path.
+    # Goes with the fix to PloneTool.changeOwnershipOf().
+    def fixOwnerTuple(obj, path):
+        old = obj.getOwnerTuple()
+        if old and old[0][-1] == 'portal_memberdata':
+            new = (['acl_users'], old[1])
+            logger.info('Repairing %s: %r -> %r' % (path, old, new))
+            obj._owner = new
+    portal.ZopeFindAndApply(portal, search_sub=True, apply_func=fixOwnerTuple)
 
 
 def to42beta1(context):
@@ -9,3 +23,7 @@ def to42beta1(context):
     """
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v42:to42beta1')
 
+
+def to42beta1_owner_tuples(context):
+    portal = getToolByName(context, 'portal_url').getPortalObject()
+    fixOwnerTuples(portal)
