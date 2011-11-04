58cc2b4146b3a32230f05b2cb1887c12a243a801
tdesvenain <thomas.desvenain@gmail.com>
2011-11-04 01:43:15 +0100

    upgrade plone_lexicon case normalizer and reindex ZCTextIndex indexes

---
+++ plone/app/upgrade/v43/alphas.py
@@ -0,0 +1,20 @@
+import logging
+
+from Products.CMFPlone.utils import getToolByName
+from plone.app.upgrade.utils import loadMigrationProfile
+
+from Products.ZCTextIndex.interfaces import IZCTextIndex
+
+logger = logging.getLogger('plone.app.upgrade')
+
+
+def upgradeToI18NCaseNormalizer(context):
+    """Upgrade lexicon to I18N Case Normalizer
+    """
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v43:upgradeToI18NCaseNormalizer')
+    catalog = getToolByName(context, 'portal_catalog')
+    for index in catalog.Indexes.objectValues():
+        if IZCTextIndex.providedBy(index):
+            logger.info("Reindex %s index with I18N Case Normalizer", index.getId())
+            catalog.reindexIndex(index.getId(), context.REQUEST)
+        pass
