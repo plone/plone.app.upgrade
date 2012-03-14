65fb9b6beb69d7752adaa8c57f37d5b0c9de7ccb
tdesvenain <thomas.desvenain@gmail.com>
2012-03-15 00:20:42 +0100

    add a test for upgrade case normalizer

---
+++ plone/app/upgrade/v43/tests.py
@@ -0,0 +1,24 @@
+import unittest
+from plone.app.upgrade.tests.base import FunctionalUpgradeTestCase
+from Products.CMFCore.utils import getToolByName
+from plone.app.upgrade.tests.base import MigrationTest
+from plone.app.upgrade.utils import loadMigrationProfile
+
+import alphas
+
+class TestMigrations_v4_3alpha1(MigrationTest):
+
+    profile = 'profile-plone.app.upgrade.v43:upgradeToI18NCaseNormalizer'
+
+    def testProfile(self):
+        # This tests the whole upgrade profile can be loaded
+        loadMigrationProfile(self.portal, self.profile)
+        self.failUnless(True)
+
+    def testUpgradeToI18NCaseNormalizer(self):
+        ctool = self.portal.portal_catalog
+        import pdb; pdb.set_trace()
+        self.assertEqual(ctool.plone_lexicon._pipeline[1].__class__.__name__, 'CaseNormalizer')
+        alphas.upgradeToI18NCaseNormalizer(self.portal.portal_setup)
+        self.assertEqual(ctool.plone_lexicon._pipeline[1].__class__.__name__, 'I18NNormalizer')
+        self.failUnless(len(ctool.searchResults(SearchableText="welcome")) > 0)
