37a242268b820f1761fed9964d576ff7116d9c8a
tdesvenain <thomas.desvenain@gmail.com>
2012-04-12 10:40:09 +0200

    pyflakes, pdb

--- plone/app/upgrade/v43/tests.py
+++ plone/app/upgrade/v43/tests.py
@@ -1,10 +1,8 @@
-import unittest
-from plone.app.upgrade.tests.base import FunctionalUpgradeTestCase
-from Products.CMFCore.utils import getToolByName
 from plone.app.upgrade.tests.base import MigrationTest
 from plone.app.upgrade.utils import loadMigrationProfile
 
 import alphas
+
 
 class TestMigrations_v4_3alpha1(MigrationTest):
 
@@ -17,7 +15,6 @@ class TestMigrations_v4_3alpha1(MigrationTest):
 
     def testUpgradeToI18NCaseNormalizer(self):
         ctool = self.portal.portal_catalog
-        import pdb; pdb.set_trace()
         self.assertEqual(ctool.plone_lexicon._pipeline[1].__class__.__name__, 'CaseNormalizer')
         alphas.upgradeToI18NCaseNormalizer(self.portal.portal_setup)
         self.assertEqual(ctool.plone_lexicon._pipeline[1].__class__.__name__, 'I18NNormalizer')
