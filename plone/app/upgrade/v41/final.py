25db61c73729ec25fa524bb681fd03912f8cc614
Eric Steele <ericsteele47@gmail.com>
2012-06-29 15:42:09 -0400

    Add 4.1.6 profile.
    Conflicts:
    
    	CHANGES.txt
    	plone/app/upgrade/v41/configure.zcml

--- plone/app/upgrade/v41/final.py
+++ plone/app/upgrade/v41/final.py
@@ -70,3 +70,7 @@ def to415_discussion(context):
 
 def to415(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:4.1.4-4.1.5')
+
+
+def to416(context):
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v41:4.1.5-4.1.6')
