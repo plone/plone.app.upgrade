98763edef032cf66e34612f032ad6e2cee121095
Eric Steele <ericsteele47@gmail.com>
2012-06-29 10:31:15 -0400

    Move 'rc3' to 'final'. Add upgrades for http://git.io/Ap2GKQ

--- plone/app/upgrade/v42/betas.py
+++ plone/app/upgrade/v42/betas.py
@@ -5,7 +5,6 @@ from AccessControl.Permission import Permission
 from plone.app.upgrade.utils import loadMigrationProfile
 from plone.app.upgrade.utils import installOrReinstallProduct
 from Products.CMFCore.utils import getToolByName
-from Products.GenericSetup.upgrade import _upgrade_registry
 
 logger = logging.getLogger('plone.app.upgrade')
 
@@ -84,28 +83,3 @@ def to42rc2(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v42:to42rc2')
 
 
-def to42rc3_cmfeditions_registry_bases(context):
-    """install the component registry bases modifier."""
-    # for portals upgraded from older versions, the cmfeditions
-    # profile isn't installed and so has no installed version number.
-    # this applies a necessary upgrade step but also establishes a
-    # version for the profile
-    setup = getToolByName(context, 'portal_setup')
-
-    # Copied from Products.GenericSetup.tool.SetupTool.manage_doUpgrades
-    logger = logging.getLogger('GenericSetup')
-    steps_to_run = ['36634937']
-    profile_id = 'Products.CMFEditions:CMFEditions'
-    step = None
-    for step_id in steps_to_run:
-        step = _upgrade_registry.getUpgradeStep(profile_id, step_id)
-        if step is not None:
-            step.doStep(setup)
-            msg = "Ran upgrade step %s for profile %s" % (step.title,
-                                                          profile_id)
-            logger.log(logging.INFO, msg)
-
-    # We update the profile version to the last one we have reached
-    # with running an upgrade step.
-    if step and step.dest is not None and step.checker is None:
-        setup.setLastVersionForProfile(profile_id, step.dest)
