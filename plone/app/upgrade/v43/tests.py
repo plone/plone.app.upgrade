99ad0770e9b910f91b6960e08ae815887bf2eebb
Tom Gross <itconsense@gmail.com>
2012-07-18 07:01:22 +0200

    added TinyMCE upgrade step

--- plone/app/upgrade/v43/tests.py
+++ plone/app/upgrade/v43/tests.py
@@ -28,3 +28,27 @@ class TestMigrations_v4_3alpha1(MigrationTest):
         alphas.upgradeToI18NCaseNormalizer(self.portal.portal_setup)
         self.assertEqual(ctool.plone_lexicon._pipeline[1].__class__.__name__, 'I18NNormalizer')
         self.failUnless(len(ctool.searchResults(SearchableText="welcome")) > 0)
+
+    def testUpgradeTinyMCE(self):
+        jstool = getToolByName(self.portal, 'portal_javascripts')
+        jsresourceids = jstool.getResourceIds()
+
+        self.assertIn('jquery.tinymce.js', jsresourceids)
+        for ne in ['tiny_mce.js', 'tiny_mce_init.js']:
+            self.assertNotIn(ne, jsresourceids, ne)
+
+        ksstool = getToolByName(self.portal, 'portal_kss')
+        kssresourceids = ksstool.getResourceIds()
+        self.assertNotIn('++resource++tinymce.kss/tinymce.kss',
+                         kssresourceids)
+
+        # xxx check for viewlet in plone.htmlhead
+        from zope.component import getAdapters, queryMultiAdapter
+        from zope.viewlet.interfaces import IViewlet
+        from zope.contentprovider.interfaces import IContentProvider
+        plone_view = queryMultiAdapter((self.portal, request), name="plone")
+        manager = queryMultiAdapter(
+                    (self.portal, request, plone_view), IContentProvider, 'plone.htmlhead')
+        viewlets = getAdapters(
+                (manager.context, manager.request, manager.__parent__, manager), IViewlet)
+        print viewlets
