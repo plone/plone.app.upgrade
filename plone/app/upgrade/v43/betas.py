3e2f0ca6ea73450155847700a423687bfc0f6eb2
Eric Steele <ericsteele47@gmail.com>
2013-01-17 23:01:44 -0500

    Add plonetheme.sunburst 1.4 upgrade step.

--- plone/app/upgrade/v43/betas.py
+++ plone/app/upgrade/v43/betas.py
@@ -2,3 +2,10 @@ from plone.app.upgrade.utils import loadMigrationProfile
 
 def to43beta2(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v43:to43beta2')
+
+
+def upgradeSunburst(context):
+    """ Upgrade plonetheme.sunburst to version 1.4
+    """
+    from plonetheme.sunburst.setuphandlers import upgrade_step_2_3
+    upgrade_step_2_3(context)
