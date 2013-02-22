43447ac61b9f566908229141a03dc3359dda394f
tdesvenain <thomas.desvenain@gmail.com>
2013-02-22 18:10:17 +0100

    Upgrade plone lexicon using I18N Case normalizer works back

--- plone/app/upgrade/v43/betas.py
+++ plone/app/upgrade/v43/betas.py
@@ -1,7 +1,12 @@
 from plone.app.upgrade.utils import loadMigrationProfile
+from plone.app.upgrade.v43.alphas import upgradeToI18NCaseNormalizer
 
 def to43beta2(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v43:to43beta2')
+
+def to43beta3(context):
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v43:to43beta3')
+    upgradeToI18NCaseNormalizer(context)
 
 
 def upgradeSunburst(context):
