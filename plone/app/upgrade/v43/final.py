470bbe6ed60d2a996acdecff21b6e404047e7145
Cédric Messiant <cedric.messiant@gmail.com>
2013-06-05 11:15:42 +0200

    Fix bug in upgrade step if 4.3.1 pending has been tested

--- plone/app/upgrade/v43/final.py
+++ plone/app/upgrade/v43/final.py
@@ -10,6 +10,9 @@ def addScalingQualitySetting(context):
     sptool = getToolByName(context, 'portal_properties')
     imaging_properties = sptool.imaging_properties
     if not imaging_properties.hasProperty('quality'):
+        if 'quality' in imaging_properties.__dict__:
+            # fix bug if 4.3.1 pending has been tested
+            del imaging_properties.quality
         imaging_properties.manage_addProperty('quality', 88, 'int')
         logger.log(logging.INFO,
                    "Added 'quality' property to imaging_properties.")
