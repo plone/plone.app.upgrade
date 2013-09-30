b43fb8e4daa766a06e21b012404b17c8303e8ff3
tisto <tisto@plone.org>
2013-09-30 21:50:20 +0200

    Add undeclared Products.TinyMCE dependency.

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(name='plone.app.upgrade',
         'Products.PlonePAS',
         'Products.PluggableAuthService',
         'Products.PortalTransforms',
+        'Products.TinyMCE',
         'Products.ResourceRegistries',
         'Products.SecureMailHost', # For migration only, when can we remove this?
         'Products.ZCatalog >= 2.13.4',
