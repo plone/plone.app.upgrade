e12792e5a7fcd3ddfd93e7de1c867de3fe18a252
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2014-02-13 17:08:38 +0100

    add upgrade step for hiding footer's viewlets and adding portlets

--- plone/app/upgrade/v50/tests.py
+++ plone/app/upgrade/v50/tests.py
@@ -1,4 +1,11 @@
+from zope.component import getUtility
+from zope.component import getSiteManager
+from zope.component import getMultiAdapter
+from plone.portlets.interfaces import IPortletManager
 from Products.CMFCore.utils import getToolByName
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from plone.app.viewletmanager.interfaces import IViewletSettingsStorage
+
 
 from plone.app.upgrade.tests.base import MigrationTest
 
@@ -32,3 +39,23 @@ class PASUpgradeTest(MigrationTest):
         alphas.lowercase_email_login(self.portal)
         self.assertEqual(pas.getProperty('login_transform'), 'lower')
         self.assertEqual(pas.getUserById('JOE').getUserName(), 'joe')
+
+    def test_footer_portletmanager_added(self):
+        sm = getSiteManager(self.portal)
+        registrations = [r.name for r in sm.registeredUtilities()
+                         if IPortletManager == r.provided]
+        self.assertTrue('plone.footerportlets' in registrations)
+        manager = getUtility(IPortletManager, name='plone.footerportlets', context=self.portal)
+        mapping = getMultiAdapter((self.portal, manager), IPortletAssignmentMapping)
+        self.assertEqual(['footer', 'actions', 'colophon'], mapping.keys())
+
+    def test_footer_viewlets_hidden(self):
+
+        storage = getUtility(IViewletSettingsStorage)
+        manager = "plone.portalfooter"
+        skinname = self.portal.getCurrentSkinName()
+
+        hidden_viewlets = storage.getHidden(manager, skinname)
+
+        self.assertEqual((u'plone.colophon', u'plone.site_actions'),
+                         hidden_viewlets)
