b97989b13e7321148e8f234115faa19cc4b354fe
Maurits van Rees <maurits@vanrees.org>
2017-04-05 17:05:33 +0200

    Fixed title and description of plone.resource.maxage.
    This had the title and description from shared maxage, due to a wrong reference.
    See https://github.com/plone/Products.CMFPlone/issues/1989
    
    This is run when upgrading to Plone 5.0.8 and 5.1b4.

--- plone/app/upgrade/v50/final.py
+++ plone/app/upgrade/v50/final.py
@@ -101,3 +101,32 @@ def to503(context):
 def to507(context):
     """5.0.6 -> 5.0.7"""
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v50:to507')
+
+
+def fix_double_smaxage(context):
+    """Fix caching definition.
+
+    plone.resource.maxage has title and description from shared maxage.
+    See https://github.com/plone/Products.CMFPlone/issues/1989
+    """
+    from plone.registry.interfaces import IPersistentField
+    from plone.registry.record import Record
+    from plone.registry import field
+    from plone.registry import FieldRef
+    from zope.component import queryAdapter
+    registry = getUtility(IRegistry)
+    # If these three registry records are not defined,
+    # we do no fix.
+    maxage = 'plone.app.caching.strongCaching.plone.resource.maxage'
+    def_maxage = 'plone.app.caching.strongCaching.maxage'
+    def_smaxage = 'plone.app.caching.strongCaching.smaxage'
+    for name in (maxage, def_maxage, def_smaxage):
+        if name not in registry:
+            return
+    if registry.records[maxage].field.recordName != def_smaxage:
+        # no fix needed
+        return
+    field_ref = FieldRef(def_maxage, registry.records[def_maxage].field)
+    old_value = registry[maxage]
+    registry.records[maxage] = Record(field_ref, old_value)
+    logger.info('Fixed {} to refer to {}.'.format(maxage, def_maxage))
