790b1ae45e524ab3f3820bb9fe001165839fef5f
MrTango <md@derico.de>
2017-06-23 20:07:44 +0300

    Fix upgrade step for ISocialMediaSchema

--- plone/app/upgrade/v51/betas.py
+++ plone/app/upgrade/v51/betas.py
@@ -5,8 +5,8 @@ from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import ISearchSchema
 from zope.component import getUtility
-
 import logging
+import unicodedata
 
 
 logger = logging.getLogger('plone.app.upgrade')
@@ -124,15 +124,26 @@ def update_social_media_fields(context):
     field values as they are now declared as ASCIILine instead of
     TextLine.
     """
+    twitter_username = ''
+    facebook_app_id = ''
+    facebook_username = ''
     from Products.CMFPlone.interfaces.controlpanel import ISocialMediaSchema
     registry = getUtility(IRegistry)
     settings = registry.forInterface(ISocialMediaSchema, prefix='plone')
     if settings.twitter_username:
-        settings.twitter_username = str(settings.twitter_username)
+        twitter_username = unicodedata.normalize(
+            'NFKD', settings.twitter_username).encode('ascii', 'ignore')
     if settings.facebook_app_id:
-        settings.facebook_app_id = str(settings.facebook_app_id)
+        facebook_app_id = unicodedata.normalize(
+            'NFKD', settings.facebook_app_id).encode('ascii', 'ignore')
     if settings.facebook_username:
-        settings.facebook_username = str(settings.facebook_username)
+        facebook_username = unicodedata.normalize(
+            'NFKD', settings.facebook_username).encode('ascii', 'ignore')
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v51:to51beta4')
+    settings.twitter_username = twitter_username
+    settings.facebook_app_id = facebook_app_id
+    settings.facebook_username = facebook_username
+
     logger.log(logging.INFO, 'Field types updated on social media schema')
 
 
