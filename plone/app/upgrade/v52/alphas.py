82f13e2dca15384c1a9c874198cfda950553f223
esteele <eric@esteele.net>
2017-10-21 13:24:17 +0200

    Add upgrade space for 5.2

---
+++ plone/app/upgrade/v52/alphas.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from plone.app.upgrade.utils import loadMigrationProfile
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
+import logging
+
+
+logger = logging.getLogger('plone.app.upgrade')
+
+def to52alpha1(context):
+    loadMigrationProfile(context, 'profile-plone.app.upgrade.v52:to52alpha1')
+    _fix_typo_in_toolbar_less_variable(context)
+
