0adaffc9a738134db95329e42f08e975599434fb
esteele <eric@esteele.net>
2017-11-26 21:25:15 -0500

    Preparing release 2.0.9
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-2.0.9 (unreleased)
+2.0.9 (2017-11-26)
 ------------------
 
 New features:
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.0.9.dev0'
+version = '2.0.9'
 
 setup(
     name='plone.app.upgrade',
