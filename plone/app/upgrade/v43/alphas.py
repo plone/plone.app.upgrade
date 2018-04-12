4730214ecabbbf1f6e10de691e67cdf86fca05d8
Maurits van Rees <maurits@vanrees.org>
2018-04-13 01:45:06 +0200

    Made several functions less complex by splitting them.
    This was the code-analysis report beforehand:
    
    $ bin/code-analysis
    Check clean lines....................[ OK ] in 0.132s
    Flake8..........................[ FAILURE ] in 1.715s
    plone/app/upgrade/utils.py:266:1: C901 'updateIconsInBrains' is too complex (19)
    plone/app/upgrade/v41/alphas.py:30:1: C901 'add_siteadmin_role' is too complex (14)
    plone/app/upgrade/v40/alphas.py:179:1: C901 'migrateActionIcons' is too complex (17)
    plone/app/upgrade/v40/betas.py:43:1: C901 'updateSafeHTMLConfig' is too complex (11)
    plone/app/upgrade/v40/betas.py:220:1: C901 'fix_cataloged_interface_names' is too complex (11)
    plone/app/upgrade/v43/alphas.py:17:1: C901 'reindex_sortable_title' is too complex (11)
    plone/app/upgrade/v43/alphas.py:137:1: C901 'upgradeSyndication' is too complex (21)
    plone/app/upgrade/v43/final.py:238:1: C901 'removeFakeKupu' is too complex (15)
    plone/app/upgrade/v50/betas.py:495:1: C901 'to50rc3' is too complex (19)
    Check MANIFEST.in....................[ OK ] in 2.107s
    
    And afterwards:
    
    $ bin/code-analysis
    Check clean lines....................[ OK ] in 0.133s
    Flake8...............................[ OK ] in 1.713s
    Check MANIFEST.in....................[ OK ] in 2.163s

--- plone/app/upgrade/v43/alphas.py
+++ plone/app/upgrade/v43/alphas.py
@@ -39,13 +39,19 @@ def reindex_sortable_title(context):
             else:
                 change.extend(list(keys))
     pghandler.finish()
-    update_metadata = 'sortable_title' in _catalog.schema
+    # flake8 complained that reindex_sortable_title is too complex (11).
+    # So we split it in two.
+    _update_sortable_title(_catalog, change)
+
+
+def _update_sortable_title(catalog, change):
+    update_metadata = 'sortable_title' in catalog.schema
     pghandler = ZLogHandler(1000)
     logger.info('Updating sortable_title index')
     pghandler.init('Updating sortable_title index', len(change))
     for i, rid in enumerate(change):
         pghandler.report(i)
-        brain = _catalog[rid]
+        brain = catalog[rid]
         try:
             obj = brain.getObject()
         except (AttributeError, KeyError):
@@ -134,33 +140,34 @@ def to43alpha1(context):
     upgradePloneAppJQuery(context)
 
 
+def _getDexterityFolderTypes(portal):
+    try:
+        from plone.dexterity.interfaces import IDexterityFTI
+        from plone.dexterity.utils import resolveDottedName
+        from Products.CMFPlone.interfaces.syndication import ISyndicatable
+    except ImportError:
+        return set([])
+
+    portal_types = getToolByName(portal, 'portal_types')
+    types = [fti for fti in portal_types.listTypeInfo() if
+             IDexterityFTI.providedBy(fti)]
+
+    ftypes = set([])
+    for _type in types:
+        klass = resolveDottedName(_type.klass)
+        if ISyndicatable.implementedBy(klass):
+            ftypes.add(_type.getId())
+    return ftypes
+
+
 def upgradeSyndication(context):
     from zope.component import getUtility, getSiteManager
     from plone.registry.interfaces import IRegistry
     from Products.CMFCore.interfaces import ISyndicationTool
-    from Products.CMFPlone.interfaces.syndication import ISyndicatable
     from Products.CMFPlone.interfaces.syndication import (
-        ISiteSyndicationSettings, IFeedSettings)
+        ISiteSyndicationSettings)
 
     portal = getToolByName(context, 'portal_url').getPortalObject()
-
-    def getDexterityFolderTypes():
-        try:
-            from plone.dexterity.interfaces import IDexterityFTI
-            from plone.dexterity.utils import resolveDottedName
-        except ImportError:
-            return set([])
-
-        portal_types = getToolByName(portal, 'portal_types')
-        types = [fti for fti in portal_types.listTypeInfo() if
-                 IDexterityFTI.providedBy(fti)]
-
-        ftypes = set([])
-        for _type in types:
-            klass = resolveDottedName(_type.klass)
-            if ISyndicatable.implementedBy(klass):
-                ftypes.add(_type.getId())
-        return ftypes
 
     logger.info('Migrating syndication tool')
     registry = getUtility(IRegistry)
@@ -169,6 +176,9 @@ def upgradeSyndication(context):
     # available
     try:
         old_synd_tool = portal.portal_syndication
+    except AttributeError:
+        pass
+    else:
         try:
             synd_settings.allowed = old_synd_tool.isAllowed
         except AttributeError:
@@ -178,39 +188,47 @@ def upgradeSyndication(context):
         except AttributeError:
             pass
         portal.manage_delObjects(['portal_syndication'])
-    except AttributeError:
-        pass
+
     sm = getSiteManager()
     sm.unregisterUtility(provided=ISyndicationTool)
+    # flake8 complained that upgradeSyndication is too complex (11).
+    # So we split it in two.
+    _update_syndication_info(portal)
+
+
+def _update_syndication_info(portal):
     # now, go through all containers and look for syndication_info
     # objects
+    from Products.CMFPlone.interfaces.syndication import IFeedSettings
+    from Products.CMFPlone.interfaces.syndication import ISyndicatable
     catalog = getToolByName(portal, 'portal_catalog')
     # get all folder types from portal_types
     at_tool = getToolByName(portal, 'archetype_tool')
     folder_types = set([])
     for _type in at_tool.listPortalTypesWithInterfaces([ISyndicatable]):
         folder_types.add(_type.getId())
-    folder_types = folder_types | getDexterityFolderTypes()
+    folder_types = folder_types | _getDexterityFolderTypes(portal)
     for brain in catalog(portal_type=tuple(folder_types)):
         try:
             obj = brain.getObject()
         except (AttributeError, KeyError):
             continue
-        if 'syndication_information' in obj.objectIds():
-            # just having syndication info object means
-            # syndication is enabled
-            info = obj.syndication_information
-            try:
-                settings = IFeedSettings(obj)
-            except TypeError:
-                continue
-            settings.enabled = True
-            try:
-                settings.max_items = info.max_items
-            except AttributeError:
-                pass
-            settings.feed_types = ('RSS',)
-            obj.manage_delObjects(['syndication_information'])
+        if 'syndication_information' not in obj.objectIds():
+            return
+        # just having syndication info object means
+        # syndication is enabled
+        info = obj.syndication_information
+        try:
+            settings = IFeedSettings(obj)
+        except TypeError:
+            continue
+        settings.enabled = True
+        try:
+            settings.max_items = info.max_items
+        except AttributeError:
+            pass
+        settings.feed_types = ('RSS',)
+        obj.manage_delObjects(['syndication_information'])
 
 
 def to43alpha2(context):
