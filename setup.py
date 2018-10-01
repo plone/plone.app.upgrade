57dee965641c70e4911a88601cf598b456be124f
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:36:11 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     keywords='Plone upgrade migration',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.app.upgrade',
+    url='https://pypi.org/project/plone.app.upgrade',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app'],
