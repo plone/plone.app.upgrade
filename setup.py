331ce90d87b1d20abf197652b1702507ed9be625
Maurits van Rees <maurits@vanrees.org>
2019-02-06 11:50:40 +0100

    Removed Plone 4.3 classifier from setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
