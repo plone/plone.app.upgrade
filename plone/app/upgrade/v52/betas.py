92d6df725d106234709b8d161827944dd44b5790
Jan Mevissen <mevissen@interaktiv.de>
2019-02-09 16:33:13 +0100

    Remove interface indexes from relation catalog

--- plone/app/upgrade/v52/betas.py
+++ plone/app/upgrade/v52/betas.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from Products.CMFCore.utils import getToolByName
 from plone.app.upgrade.utils import loadMigrationProfile
+from zc.relation.interfaces import ICatalog
+from zope import component
+from zope.intid.interfaces import IntIdMissingError
+
 
 import logging
 
@@ -25,6 +29,26 @@ def add_exclude_from_nav_index(context):
         catalog.manage_reindexIndex(ids=indexables)
 
 
+def remove_interface_indexes_from_relations_catalog():
+    """ remove unused interface indexes from relations catalog """
+    catalog = component.queryUtility(ICatalog)
+    indexes_to_remove = [
+        'from_interfaces_flattened',
+        'to_interfaces_flattened'
+    ]
+    for index_to_remove in indexes_to_remove:
+        if index_to_remove in catalog._name_TO_mapping:
+            catalog.removeValueIndex(index_to_remove)
+
+    for rel in catalog:
+        catalog.unindex(rel)
+        try:
+            catalog.index(rel)
+        except IntIdMissingError:
+            logger.warn('Broken relation removed.')
+
+
 def to52beta1(context):
     loadMigrationProfile(context, 'profile-plone.app.upgrade.v52:to52beta1')
     add_exclude_from_nav_index(context)
+    remove_interface_indexes_from_relations_catalog()
