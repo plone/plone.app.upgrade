2c82ffb957bca0d14d760ff73cd094a0b9f1febe
ale-rt <alessandro.pisa@gmail.com>
2021-02-11 16:59:03 +0100

    Make the Plone Site a dexterity container
    Refs. https://github.com/plone/Products.CMFPlone/issues/2454

--- plone/app/upgrade/v60/alphas.py
+++ plone/app/upgrade/v60/alphas.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from plone.app.upgrade.utils import loadMigrationProfile
+from zope.component.hooks import getSite
 
 import logging
 
 
-logger = logging.getLogger('plone.app.upgrade')
+logger = logging.getLogger("plone.app.upgrade")
 
 
 def to60alpha1(context):
-    loadMigrationProfile(context, 'profile-plone.app.upgrade.v60:to60alpha1')
+    loadMigrationProfile(context, "profile-plone.app.upgrade.v60:to60alpha1")
 
 
 def remove_temp_folder(context):
@@ -34,3 +35,26 @@ def remove_temp_folder(context):
         del app._mount_points[broken_id]
         app._p_changed = True
         logger.info("Removed %s from Zope root _mount_points.", broken_id)
+
+
+def make_site_dx(context):
+    """Make the Plone Site a dexterity container"""
+    portal = getSite()
+
+    if portal._tree is not None:
+        # We assume the object has been already initialized
+        return
+
+    portal._initBTrees()
+
+    for obj_meta in portal._objects:
+        obj_id = obj_meta["id"]
+        logger.info("Migrating object %r", obj_id)
+        # Load the content object ...
+        obj = portal.__dict__.pop(obj_id)
+        # ...and insert it into the btree.
+        # Use _setOb so we don't reindex stuff: the paths stay the same.
+        portal._setOb(obj_id, obj)
+
+    delattr(portal, "_objects")
+    portal._p_changed = True
