ac32e34b932c9729fe15490793636802884cfdf2
ale-rt <alessandro.pisa@gmail.com>
2022-08-25 10:15:37 +0200

    Add a timezone property to portal memberdata if it is missing
    Fixes #295

--- plone/app/upgrade/v60/betas.py
+++ plone/app/upgrade/v60/betas.py
@@ -1,3 +1,12 @@
+from Products.CMFPlone.utils import getToolByName
 import logging
 
 logger = logging.getLogger("plone.app.upgrade")
+
+
+def add_the_timezone_property(context):
+    """Ensure that the portal_memberdata tool has the timezone property."""
+    portal_memberdata = getToolByName(context, "portal_memberdata")
+    if portal_memberdata.hasProperty("timezone"):
+        return
+    portal_memberdata.manage_addProperty("timezone", "", "string")
