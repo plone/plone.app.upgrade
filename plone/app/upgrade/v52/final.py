f4b222180cc9aa3c35fa7038aab59c9fcda9ab60
Maurits van Rees <maurits@vanrees.org>
2023-01-16 22:13:47 +0100

    Apply weak caching to GET requests of content with application/json.
    This is the upgrade step belonging to https://github.com/plone/plone.app.caching/pull/111
    See https://github.com/plone/plone.rest/issues/73 for the full story.

--- plone/app/upgrade/v52/final.py
+++ plone/app/upgrade/v52/final.py
@@ -275,3 +275,34 @@ def add_the_timezone_property(context):
     if portal_memberdata.hasProperty("timezone"):
         return
     portal_memberdata.manage_addProperty("timezone", "", "string")
+
+
+def add_get_application_json_to_weak_caching(context):
+    """Add GET application/json for content to weak caching.
+
+    See https://github.com/plone/plone.rest/issues/73#issuecomment-1384298492
+    We want to get this in the templateRulesetMapping setting of the registry:
+
+        <element key="GET_application_json_">plone.content.folderView</element>
+    """
+    registry = getUtility(IRegistry)
+    try:
+        from plone.app.caching.interfaces import IPloneCacheSettings
+    except ImportError:
+        # plone.app.caching is optional.
+        return
+
+    try:
+        settings = registry.forInterface(IPloneCacheSettings)
+    except KeyError:
+        # It is available, but not activated.  Nothing to do.
+        return
+    mapping = settings.templateRulesetMapping
+    key = "GET_application_json_"
+    if key in mapping:
+        # already set, do not change
+        return
+    mapping[key] = "plone.content.folderView"
+    # Note: if we edit templateRulesetMapping, our change will not be persisted,
+    # because it is a simple dict.  We have to set the entire mapping.
+    settings.templateRulesetMapping = mapping
