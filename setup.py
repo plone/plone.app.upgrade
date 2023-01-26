0248742cc266ee0148d27b47c827f6e65e1c5261
Maurits van Rees <maurits@vanrees.org>
2023-01-26 17:04:06 +0100

    Add python_requires.
    [ci skip]

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     namespace_packages=['plone', 'plone.app'],
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     extras_require=dict(
         test=[
             'zope.site',
