79b96ee9127eac28f07991a15c72eeb8ec1753d0
Peter Mathis <peter.mathis@kombinat.at>
2025-09-30 13:35:04 +0200

    Update TinyMCE toolbar and menu button name.

---
+++ src/plone/app/upgrade/v62/alpha.py
@@ -0,0 +1,22 @@
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
+
+def update_tinymce_toolbar_menu_styles(context):
+    """
+    the interface definition of plone.base.interfaces.controlpanel.ITinyMCEPluginSchema changed
+    the field `menubar` is now a TextField
+    the registry record must be converted
+    """
+
+    registry = getUtility(IRegistry)
+
+    breakpoint()
+    # rename "styleselect" to "styles"
+    mce_toolbar = registry.records["plone.toolbar"].value
+    registry.records["plone.toolbar"] = mce_toolbar.replace("styleselect", "styles")
+
+    # rename "formats" to "styles"
+    mce_menu = registry.records["plone.menu"].value
+    mce_menu["format"]["items"] = mce_menu["format"]["items"].replace("formats", "styles")
+    registry.records["plone.menu"] = mce_menu
