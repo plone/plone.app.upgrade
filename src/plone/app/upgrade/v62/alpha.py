fa3b1ab712de5186f788d15475c233eb023e2756
Johannes Raggam <thetetet@gmail.com>
2026-02-28 18:27:41 +0100

    6.2: Remove remaining deprecated Plone 5 resource registry records.

--- src/plone/app/upgrade/v62/alpha.py
+++ src/plone/app/upgrade/v62/alpha.py
@@ -5,6 +5,7 @@ from zope.component import getUtility
 
 import json
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -51,3 +52,33 @@ def install_plone_app_layout(context):
     if installer.is_product_installed("plone.app.layout"):
         return
     installer.install_product("plone.app.layout")
+
+
+def cleanup_resource_registry(self):
+    """Remove remaining deprecated Plone 5 resource registry records."""
+    # Regex for deprecated resource registry keys.
+    resource_regex = re.compile(
+        r"plone.bundles.*("
+        "compile|"
+        "develop_css|"
+        "develop_javascript|"
+        "last_compilation|"
+        "merge_with|"
+        "resources|"
+        "stub_js_modules"
+        ")"
+    )
+    registry = getUtility(IRegistry)
+
+    # Create a static list of registry keys to not operate on a live iterator
+    # which changes size when deleting keys.
+    keys = list(registry.records.keys())
+    for key in keys:
+        # Remove "plone.resources", "plone.lessvariables" and deprecated bundle keys.
+        if (
+            "plone.resources" == key[:15]
+            or key == "plone.lessvariables"
+            or resource_regex.match(key)
+        ):
+            del registry.records[key]
+            logger.info("Removed deprecated registry record %s.", key)
