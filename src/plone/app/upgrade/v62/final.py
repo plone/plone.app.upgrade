ad49a9936afd109bc151d9506f8db0cbaa000a14
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-07-02 15:58:01 +0200

    flatten the ifs

--- src/plone/app/upgrade/v62/final.py
+++ src/plone/app/upgrade/v62/final.py
@@ -12,26 +12,30 @@ def manage_comments_icon(context):
     """
 
     installer = get_installer(context)
-    if installer.is_product_installed("plone.app.discussion"):
-        portal_actions = getToolByName(context, "portal_actions")
-        user_actions = getattr(portal_actions, "user", None)
-        if user_actions is not None:
-            review_comments_action = getattr(user_actions, "review-comments", None)
-            if review_comments_action is not None:
-                # Check whether the action keeps its original state
-                if (
-                    review_comments_action.icon_expr
-                    == "string:${globals_view/navigationRootUrl}/discussionitem_icon.png"
-                ):
-                    review_comments_action.icon_expr = "string:chat"
-                    logger.info("Manage comments action icon modified")
-                else:
-                    logger.info(
-                        "Manage comments action icon was modified by the user. Nothing is done."
-                    )
-            else:
-                logger.info("There is no manage comments action. Nothing is done.")
-        else:
-            logger.info("There are no user actions. Nothing si done.")
-    else:
+    if not installer.is_product_installed("plone.app.discussion"):
         logger.info("plone.app.discussion is not installed. Nothing is done.")
+        return
+
+    portal_actions = getToolByName(context, "portal_actions")
+    user_actions = getattr(portal_actions, "user", None)
+    if user_actions is None:
+        logger.info("There are no user actions. Nothing is done.")
+        return
+
+    review_comments_action = getattr(user_actions, "review-comments", None)
+    if review_comments_action is None:
+        logger.info("There is no manage comments action. Nothing is done.")
+        return
+
+    # Check whether the action keeps its original state
+    if (
+        review_comments_action.icon_expr
+        != "string:${globals_view/navigationRootUrl}/discussionitem_icon.png"
+    ):
+        logger.info(
+            "Manage comments action icon was modified by the user. Nothing is done."
+        )
+        return
+
+    review_comments_action.icon_expr = "string:chat"
+    logger.info("Manage comments action icon modified")
